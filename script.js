ed28fb93df9a0e44894aa1f1ef7ba00fa2aac951
shahrokh-t <tahmoores54@gmail.com>
2023-06-11 17:36:03 +1000

    add quadratic curves example

--- script.js
+++ script.js
@@ -89,6 +89,17 @@ ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2)
 
 ctx.stroke();
 
+// Quadratic curves example
+ctx.beginPath();
+ctx.moveTo(75, 25);
+ctx.quadraticCurveTo(25, 25, 25, 62.5);
+ctx.quadraticCurveTo(25, 100, 50, 100);
+ctx.quadraticCurveTo(50, 120, 30, 125);
+ctx.quadraticCurveTo(60, 120, 65, 100);
+ctx.quadraticCurveTo(125, 100, 125, 62.5);
+ctx.quadraticCurveTo(125, 25, 75, 25);
+ctx.stroke();
+
 
 ////////////////////////////////// Animation 1 - Bouncing Ball 
 const circle = {
